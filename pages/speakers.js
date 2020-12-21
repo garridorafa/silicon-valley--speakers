78f08b0bb3b34d1b94d0600dbf696287f08ba551
Rafa Garrido <draco032@gmail.com>
2020-12-20 20:14:19 -0400

    Refactor SpeakersPager with SpeakerContext

--- pages/speakers.js
+++ pages/speakers.js
@@ -4,13 +4,35 @@ import SpeakerSearchBar from '../src/components/SpeakerSearchBar/SpeakerSearchBa
 import Speakers from '../src/components/Speakers/Speakers'
 import Footer from '../src/components/Footer/Footer'
 
+import SpeakerContext from '../src/components/Speakers/SpeakerContext'
+
+
+
 function SpeakersPage() {
+
+  const speakers = [
+    {
+      imageSrc: 'speaker-component-1',
+      name: 'Douglas Crockford'
+    },
+    {
+      imageSrc: 'speaker-component-2',
+      name: 'Tamara Baker'
+    },
+    {
+      imageSrc: 'speaker-component-3',
+      name: 'Eugene Chuvyrov'
+    }
+  ];
+
   return (
     <div>
       <Header />
       <Menu />
-      <SpeakerSearchBar />
-      <Speakers />
+      <SpeakerContext.Provider value={speakers}>
+        <SpeakerSearchBar />
+        <Speakers />
+      </SpeakerContext.Provider>
       <Footer />
     </div>
   )
