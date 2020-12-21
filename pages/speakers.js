82e36f1d9a2c0ccdf931f60092a49acac65e2702
Rafa Garrido <draco032@gmail.com>
2020-12-20 21:45:29 -0400

    Add react-symple-img, tailwind and somes styles

--- pages/speakers.js
+++ pages/speakers.js
@@ -1,41 +1,10 @@
-import Header from '../src/components/Header/Header'
-import Menu from '../src/components/Menu/Menu'
-import SpeakerSearchBar from '../src/components/SpeakerSearchBar/SpeakerSearchBar'
-import Speakers from '../src/components/Speakers/Speakers'
-import Footer from '../src/components/Footer/Footer'
+import Speakers from '../src/components/Speakers/Speakers';
+import Layout from '../src/components/Layout/Layout';
 
-import SpeakerContext from '../src/components/Speakers/SpeakerContext'
-
-
-
-function SpeakersPage() {
-
-  const speakers = [
-    {
-      imageSrc: 'speaker-component-1',
-      name: 'Douglas Crockford'
-    },
-    {
-      imageSrc: 'speaker-component-2',
-      name: 'Tamara Baker'
-    },
-    {
-      imageSrc: 'speaker-component-3',
-      name: 'Eugene Chuvyrov'
-    }
-  ];
-
+export default function Page() {
   return (
-    <div>
-      <Header />
-      <Menu />
-      <SpeakerContext.Provider value={speakers}>
-        <SpeakerSearchBar />
-        <Speakers />
-      </SpeakerContext.Provider>
-      <Footer />
-    </div>
-  )
+    <Layout>
+      <Speakers />
+    </Layout>
+  );
 }
-
-export default SpeakersPage;
